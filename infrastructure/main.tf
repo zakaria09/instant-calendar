f69a178939a8519df124919978b5b711afa9ccf8
zakaria arr <zakaria.09@hotmail.co.uk>
2026-04-09 17:12:26 +0100

    move tfstate to digital ocean s3

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,6 +5,19 @@ terraform {
       version = "~> 2.0"
     }
   }
+
+backend "s3" {
+  endpoints = {
+    s3 = "https://lon1.digitaloceanspaces.com"
+  }
+  bucket                      = "instant-calendar-tf-state"
+  key                         = "terraform.tfstate"
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_s3_checksum            = true
+}
 }
 
 provider "digitalocean" {
